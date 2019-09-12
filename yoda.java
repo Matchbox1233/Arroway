b17a62c2574d6fb3d72670c4f8e0ebe1826ca0ac
Matchbox1233 <43754339+Matchbox1233@users.noreply.github.com>
2019-09-12 19:21:26 +0200

    Add files via upload

---
+++ yoda.java
@@ -0,0 +1,12 @@
+public class yoda {
+	public static void main(String[] args) {
+	String s=null;
+	if
+		/*(s.equals("yoda"))*/  //ebben az esetben áll le a NullPinterException-nal
+		("yoda".equals(s))		//Lefordul, de nem fut le, nem írja ki, hogy Yoda
+
+		{
+			System.out.println("Yoda");
+		}
+	}
+}
